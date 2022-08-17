4d3137626f76444c521927b3b1911261bc5a2d7b
ISXDora <isadora@debian-nvme>
2022-08-16 23:32:42 -0300

    teste estilização

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -3,14 +3,18 @@ import styles from './Task.module.css'
 import {Trash, Check} from 'phosphor-react'
 
 export const Task: React.FC = () => {
+  function activeLineTrought(){
+
+    
+  }
   return (
     <div className={styles.container}>
       <label>
 
       <input type="checkbox"/>
-      <span><Check size={12} weight="bold"/></span>
+      <span><Check size={12} weight="bold" onClick={activeLineTrought}/></span>
       </label>
-      <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+      <p className={styles.done}>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
       <button>
         <Trash size={16}/>
       </button>
