45bb15aa0cc17a43d1a0ad7191e955d155eb938c
ISXDora <isadora@debian-nvme>
2022-08-17 22:30:24 -0300

    chore: input do checkbox controlado

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import styles from './Task.module.css'
 import {Trash, Check} from 'phosphor-react'
 
 export const Task: React.FC = () => {
-  function activeLineTrought(){
+  const [isDone, setIsDone] = useState(false)
 
-    
+
+  const togglechecked = (event : ChangeEvent<HTMLInputElement>) => {
+   const {target} = event
+   if (target.checked) {
+     setIsDone(true)
+   }else {
+    setIsDone(false)
+   }
   }
+
   return (
     <div className={styles.container}>
-      <label>
+      <label >
 
-      <input type="checkbox"/>
-      <span><Check size={12} weight="bold" onClick={activeLineTrought}/></span>
+      <input type="checkbox" onChange={togglechecked}/>
+      <span><Check size={12} weight="bold" /></span>
       </label>
-      <p className={styles.done}>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+      <p className={isDone ? styles.done : '' } >Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
       <button>
         <Trash size={16}/>
       </button>
